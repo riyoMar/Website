06ca2faf643fbc3cc67a9d9b55017f3cdcd2578e
riyoMar <151413713+riyoMar@users.noreply.github.com>
2023-11-21 22:04:18 +0700

    Update web.php

--- website/routes/web.php
+++ website/routes/web.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-route::view('/', 'home_2');
-// Route::view('/', 'home_2');
+
+Route::view('/', 'home_2');
 
 Route::view('home', 'home_2');
 Route::view('home-1', 'home');
